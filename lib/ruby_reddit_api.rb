7d48170b908d4d92433e3d0b85f1bdcb541a5905
Justin Lewis <justin@interwebr.com>
2011-12-21 16:31:33 -0800

    cleanup

--- lib/ruby_reddit_api.rb
+++ lib/ruby_reddit_api.rb
@@ -17,5 +17,4 @@ require "ruby_reddit_api/submission"
 require "ruby_reddit_api/comment"
 require "ruby_reddit_api/message"
 require "ruby_reddit_api/message_group"
-require "ruby_reddit_api/submit"
 
